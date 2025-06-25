8d0019a69f0ccec379d51f41c13a9c723f81f218
Felipe Nobre <167920686+fanthed10@users.noreply.github.com>
2025-06-25 09:32:26 -0300

    Add files via upload

---
+++ sistema-alunos/data/alunoData.js
@@ -0,0 +1,7 @@
+let alunos = [
+    { id: 1, nome: 'João Silva', curso: 'Engenharia', ira: 8.5 },
+    { id: 2, nome: 'Maria Oliveira', curso: 'Medicina', ira: 9.2 },
+    { id: 3, nome: 'Carlos Souza', curso: 'Computação', ira: 7.8 }
+  ];
+  
+  module.exports = alunos;
